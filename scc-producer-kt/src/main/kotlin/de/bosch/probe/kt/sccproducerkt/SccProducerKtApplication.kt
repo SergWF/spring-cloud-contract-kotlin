b1152e8e4a5b1da1e868066287021a6f26e0d844
sergwf <sergwf@gmail.com>
2018-12-18 16:05:19 +0100

    adding /greeting controller

--- scc-producer-kt/src/main/kotlin/de/bosch/probe/kt/sccproducerkt/SccProducerKtApplication.kt
+++ scc-producer-kt/src/main/kotlin/de/bosch/probe/kt/sccproducerkt/SccProducerKtApplication.kt
@@ -2,11 +2,48 @@ package de.bosch.probe.kt.sccproducerkt
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
 
 @SpringBootApplication
 class SccProducerKtApplication
 
 fun main(args: Array<String>) {
-	runApplication<SccProducerKtApplication>(*args)
+    runApplication<SccProducerKtApplication>(*args)
 }
 
+data class Greeting(val name: String, val hello: String, val time: LocalDateTime)
+
+
+@RestController
+@RequestMapping("/greeting", produces = arrayOf(org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE))
+class GreetingController(val greetingService: GreetingService) {
+
+
+
+    @GetMapping
+    fun getAll(): List<Greeting> {
+        return greetingService.all()
+    }
+
+    @PostMapping
+    fun hello(@RequestParam name: String) : Greeting {
+        return greetingService.sayHello(name)
+    }
+}
+
+@Service
+class GreetingService {
+
+    val helloMap: MutableMap<String, Greeting> = HashMap()
+
+    fun sayHello(name: String): Greeting {
+        helloMap.putIfAbsent(name, Greeting(name, "hello", LocalDateTime.now()))
+        return helloMap.getValue(name)
+    }
+
+    fun all(): List<Greeting> {
+        return helloMap.values.toList()
+    }
+}
